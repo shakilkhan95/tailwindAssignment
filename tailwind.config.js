087eb1a3bfe108d9a55363e61055ccba5bf2a087
shakilkhan95 <th.shakilkhan@gmail.com>
2024-11-07 04:02:09 +0400

    Project Setup

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*'],
+  theme: {
+    extend: {
+      fontFamily: {
+        'plus-jakarta': '"Plus Jakarta Sans", sans-serif',
+        'poppins': '"Poppins", sans-serif'
+      },
+
+      colors: {
+        'main-bg-color': '#FAFAFA',
+        'main-text-color': '#181818',
+        'sec-text-color': '#072125',
+        'third-color': '#00FF84',
+        'btn-color': '#F44335'
+      }
+    },
+  },
+  plugins: [],
+}
+
